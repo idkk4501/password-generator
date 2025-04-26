2cebe98f75982104c7b255357c117c41345af574
idkk4501 <justinirvin951@gmail.com>
2025-04-25 23:15:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 print('your password is: ')
 
-chars = 'abcdefghijklmnopqrstuvwxyz/\}{)(*&^%$#@!1234567890=+_-?><,.:'
+chars = 'abcdefghijklmnopqrstuvwxyz/{)(*&^%$#@!1234567890=+_-?><,.:'
 
 password =''
 for x in range(10):
