325aea061d47becaae8401120f68a40a6428a80a
idkk4501 <justinirvin951@gmail.com>
2025-04-25 23:11:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import random
 
+print('your password is: ')
+
+pch = "abcdefghijklmnopqrstuvwxyz/\}{)(*&^%$#@!1234567890=+_-?><,.:'
+
+password = ''
+for x in range(10):
+  password += random.choice(pch)
